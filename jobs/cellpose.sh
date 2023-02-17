92cbc4f9e775f89e472911f5a393636a80be5649
Torec Luik <t.t.luik@amc.uva.nl>
2023-02-17 16:49:40 +0100

    Add IMAGE_PATH env for container images

--- jobs/cellpose.sh
+++ jobs/cellpose.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=cellpose
+#SBATCH --job-name=w_nucleisegmentation-cellpose-v1.2.2
 #SBATCH --cpus-per-task=4
 #SBATCH --time=01:00:00
 #SBATCH --mem=8GB
@@ -7,7 +7,7 @@
 #SBATCH --mail-user=t.t.luik@amsterdamumc.nl
 #SBATCH --mail-type=END,FAIL
 
-singularity run --bind $DATA_PATH/data:/data w_nucleisegmentation-cellpose-v1.2.2.simg \
+singularity run --bind $DATA_PATH/data:/data $IMAGE_PATH/w_nucleisegmentation-cellpose-v1.2.2.simg \
 	--infolder /data/in \
 	--outfolder /data/out \
 	--gtfolder /data/gt \
