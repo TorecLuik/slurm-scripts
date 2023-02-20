c509b16bcb4e8f9a0449f74a06a274e7b7f7d5e2
Torec Luik <t.t.luik@amc.uva.nl>
2023-02-20 11:46:04 +0100

    Extract version

--- jobs/cellpose.sh
+++ jobs/cellpose.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=w_nucleisegmentation-cellpose-v1.2.2
+#SBATCH --job-name=w_nucleisegmentation-cellpose
 #SBATCH --cpus-per-task=4
 #SBATCH --time=01:00:00
 #SBATCH --mem=8GB
@@ -7,10 +7,9 @@
 #SBATCH --mail-user=t.t.luik@amsterdamumc.nl
 #SBATCH --mail-type=END,FAIL
 
-singularity run --bind $DATA_PATH/data:/data $IMAGE_PATH/w_nucleisegmentation-cellpose-v1.2.2.simg \
+singularity run --bind $DATA_PATH/data:/data $IMAGE_PATH/w_nucleisegmentation-cellpose-$IMAGE_VERSION.simg \
 	--infolder /data/in \
 	--outfolder /data/out \
 	--gtfolder /data/gt \
 	--local \
-	-nmc \
-	--descriptor ~/my-scratch/descriptor.json
+	-nmc
