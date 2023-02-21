3cf2f9a91fc0849b1d0eb499d8c5a70c4f4e2315
Torec Luik <t.t.luik@amc.uva.nl>
2023-02-21 14:27:55 +0100

    Fix issues with bind in singularity

--- jobs/cellpose.sh
+++ jobs/cellpose.sh
@@ -3,13 +3,13 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --time=01:00:00
 #SBATCH --mem=8GB
-#SBATCH --output=/home/sandbox/ttluik/cellpose.log
+#SBATCH --output=cellpose.log
 #SBATCH --mail-user=t.t.luik@amsterdamumc.nl
 #SBATCH --mail-type=END,FAIL
 
-singularity run --bind $DATA_PATH/data:/data $IMAGE_PATH/w_nucleisegmentation-cellpose-$IMAGE_VERSION.simg \
-	--infolder /data/in \
-	--outfolder /data/out \
-	--gtfolder /data/gt \
+singularity run $IMAGE_PATH/w_nucleisegmentation-cellpose-$IMAGE_VERSION.simg \
+	--infolder $DATA_PATH/data/in \
+	--outfolder $DATA_PATH/data/out \
+	--gtfolder $DATA_PATH/data/gt \
 	--local \
 	-nmc
