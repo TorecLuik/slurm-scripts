128aa6b8d36783fc416f2bc8d8b05b72a0caaf68
Torec Luik <t.t.luik@amc.uva.nl>
2023-03-07 09:59:38 +0100

    Change logging

--- jobs/cellpose.sh
+++ jobs/cellpose.sh
@@ -3,9 +3,11 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --time=01:00:00
 #SBATCH --mem=8GB
-#SBATCH --output=cellpose.log
+#SBATCH --output=cellpose-%4j.log
 #SBATCH --mail-user=t.t.luik@amsterdamumc.nl
 #SBATCH --mail-type=END,FAIL
+
+echo "Running CellPose w/ $IMAGE_PATH | $IMAGE_VERSION | $DATA_PATH "
 
 singularity run $IMAGE_PATH/w_nucleisegmentation-cellpose-$IMAGE_VERSION.simg \
 	--infolder $DATA_PATH/data/in \
